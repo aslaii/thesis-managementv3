fcdbb1fe4b491637d079dff58d1a1f2f9a2804ae
Jericho Bermas <jecho@aslaii.com>
2024-03-09 20:23:25 +0800

    Update modal and view-thesis widths

--- src/sections/thesis/thesis-modal.js
+++ src/sections/thesis/thesis-modal.js
@@ -25,7 +25,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "calc(100% - 40px)",
+  width: "50%",
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
